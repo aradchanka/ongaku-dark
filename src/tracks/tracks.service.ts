50ccc7538d27e1e0b9fec2b4ca4917105287ec8a
tony <>
2025-06-09 10:35:20 +0200

    Fix infrastucture issues

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTrackInput } from './dto/create-track.input';
 import { UpdateTrackInput } from './dto/update-track.input';
+import { Track } from './entities/track.entity';
 
 @Injectable()
 export class TracksService {
   create(createTrackInput: CreateTrackInput) {
-    return 'This action adds a new track';
+    const track = new Track();
+    track.exampleField = createTrackInput.exampleField;
+    return track;
   }
 
   findAll() {
-    return `This action returns all tracks`;
+    const track = new Track();
+    track.exampleField = 1;
+    const tracks = [track];
+    return tracks;
   }
 
   findOne(id: number) {
